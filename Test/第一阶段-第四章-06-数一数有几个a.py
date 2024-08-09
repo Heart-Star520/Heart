c828ce4ced1b5aea8a1aa79ee795edb3cb3c2b8b
Heart-Star <3893402663@qq.com>
2024-08-09 15:24:38 +0800

    太简单辣

--- Test/第一阶段-第四章-06-数一数有几个a.py
+++ Test/第一阶段-第四章-06-数一数有几个a.py
@@ -1,12 +1,12 @@
 # 数一数有几个a
 
 name = "itheima is a brand of itcast" # 定义变量
-chance = 0 # 定义有几个a的变量
+count = 0 # 定义有几个a的变量
 
 for x in name:
     # 写一个if语法，如果x是a，chance就增加1
     if x == "a":
-        chance += 1
+        count += 1
 
 # 结束后打印结果
-print(f"itheima is a brand of itcast中共有：{chance}个字母a")
+print(f"itheima is a brand of itcast中共有{count}个字母a")
